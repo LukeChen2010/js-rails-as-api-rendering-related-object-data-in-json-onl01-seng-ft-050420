b6590ae5d4afd60f3196af41095f98500d1dcfcb
Luke Chen <luke.chen.2010@gmail.com>
2020-09-19 21:05:23 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,4 +1,5 @@
 class SightingsController < ApplicationController
+<<<<<<< HEAD
   def index
     sightings = Sighting.all
     render json: sightings.to_json(include: [:bird, :location])
@@ -12,4 +13,6 @@ class SightingsController < ApplicationController
       render json: { message: 'No sighting found with that id' }
     end
   end
+=======
+>>>>>>> 1ab9c9965fd9ad551d79282e8389fefdcc222791
 end
